0b77ac0832fa387e877b2b7b85b8a1309c12fecb
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-04 17:38:23 +0200

    Create modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) return null;
+  const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLDivElement;
+    target.id === "wrapper" ? onClose() : null;
+  };
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center "
+      id="wrapper"
+      onClick={handleClose}>
+      <div className="container w-1/2 flex flex-col">
+        <button
+          className="text-white text-xl place-self-end"
+          onClick={() => onClose()}>
+          X
+        </button>
+        <div className="bg-white p-2 rounded">
+          {children}
+        </div>
+      </div>
+
+
+    </div>
+  );
+};
