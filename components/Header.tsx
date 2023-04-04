06b851a2a68dca44ebf6a4ab0517201453fb1338
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-04 16:39:59 +0200

    Define styles in Header responsive component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,42 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export const Header = () => {
+  const router = useRouter();
   return (
-    <header>
-      test header
+    <header className="max-w-screen-2xl bg-gray-300 px-2 py-4 mx-auto">
+      <nav className="container flex justify-between items-center mx-auto">
+        <Link href="/" className="font-bold sm:text-xl md:text-2xl mx-5">VideoDownloader</Link>
+        <button onClick={() => console.log('click')}
+          className="mr-10 block sm:hidden">
+          <svg viewBox="0 0 100 90" width="30" height="30">
+            <rect width="100" height="15"></rect>
+            <rect y="35" width="100" height="15"></rect>
+            <rect y="70" width="100" height="15"></rect>
+            <rect y="70" width="100" height="15"></rect>
+          </svg>
+        </button>
+        <div className="hidden sm:block mx-5 text-xs sm:text-sm md:text-base">
+          <Link
+            href="/recent"
+            className={`px-4 ${router.pathname == "/recent" ?
+              "selected font-bold text-sky-500" : ""}`}>
+            Recent Videos
+          </Link>
+          <Link
+            href="/register"
+            className={`px-4 ${router.pathname == "/register" ?
+              "selected font-bold text-sky-500" : ""}`}>
+            Create Account
+          </Link>
+          <Link
+            href="/login"
+            className={`px-4 ${router.pathname == "/login" ?
+              "selected font-bold text-sky-500" : ""}`}>
+            Sign In
+          </Link>
+        </div>
+      </nav>
     </header>
   );
 };
