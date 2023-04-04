d511e218de8dc059d9af78878f71582868fa96e7
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-04 15:24:23 +0200

    Install dependencies and configure project

---
+++ jest.config.mjs
@@ -0,0 +1,9 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./"
+});
+const config = {
+  testEnvironment: "jest-environment-jsdom"
+};
+export default createJestConfig(config);
