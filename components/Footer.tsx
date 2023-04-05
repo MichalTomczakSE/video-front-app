6baedfe677103da7b54a76ee7431767dc9a835b1
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-05 12:44:28 +0200

    Add styles and display sizes for Footer Component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer>
-      test footer
+    <footer className="w-full max-w-screen-2xl bg-gray-300 px-2 py-4 text-right mx-auto">
+&copy;Michal Tomczak 2023
     </footer>
   );
 };
