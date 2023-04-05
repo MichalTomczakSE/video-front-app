904216f5df03dd18bb693438f94b207a51cb5b7c
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-05 12:42:47 +0200

    Add SmallDisplayNavigation to show menu buttons outside header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { SmallDisplayNavigation } from "@/components/SmallDisplayNavigation";
 
 export const Header = () => {
+  const [showNavigation, setShowNavigation] = useState<boolean>(false);
+
+  const handleMenuClick = () => {
+  setShowNavigation(!showNavigation);
+  }
   const router = useRouter();
   return (
-    <header className="max-w-screen-2xl bg-gray-300 px-2 py-4 mx-auto">
+    <>
+    <header className="w-full max-w-screen-2xl bg-gray-300 px-2 py-4 mx-auto">
       <nav className="container flex justify-between items-center mx-auto">
         <Link href="/" className="font-bold sm:text-xl md:text-2xl mx-5">VideoDownloader</Link>
-        <button onClick={() => console.log('click')}
+        <button onClick={() => handleMenuClick()}
           className="mr-10 block sm:hidden">
           <svg viewBox="0 0 100 90" width="30" height="30">
             <rect width="100" height="15"></rect>
@@ -16,6 +24,7 @@ export const Header = () => {
             <rect y="70" width="100" height="15"></rect>
           </svg>
         </button>
+
         <div className="hidden sm:block mx-5 text-xs sm:text-sm md:text-base">
           <Link
             href="/recent"
@@ -38,5 +47,9 @@ export const Header = () => {
         </div>
       </nav>
     </header>
+  <SmallDisplayNavigation
+    isOpen={showNavigation}
+  />
+    </>
   );
 };
