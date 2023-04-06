9490627015ec57a84d2d5ecd0ad41f620e5463a8
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-06 19:47:27 +0200

    Wrap Layout component into container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,10 @@ import { Layout } from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <div className="bg-gray-100 min-w-min">
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </div>
   );
 }
