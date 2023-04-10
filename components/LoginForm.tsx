6c4f6830d26edd9d8d475f6bf034e3420f5e9717
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-10 17:59:52 +0200

    Change input type to email

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -8,10 +8,10 @@ export const LoginForm = () => {
         <div>
           <label htmlFor="email"
           className="block mb-2 text-sm font-medium text-gray-900">
-            Your username:
+            Your email:
           </label>
           <input
-            type="text"
+            type="email"
           name="username"
           id="username"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
