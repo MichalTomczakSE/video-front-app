9ff11123e6b5c0dc2e5204ca9732e18c8e63a5e7
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-04-11 21:47:47 +0200

    Add handlers for login status

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -8,10 +8,13 @@ interface FormValues {
   username: string;
   password: string;
 }
+type authStatus = 'anonymous' | 'signed' | 'failed';
 
 export const LoginForm = ({ createAccount }: LoginFromProps) => {
-  const [isloading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [formData, setFormData] = useState<FormValues>({ username: "", password: "" });
+  const [userStatus, setUserStatus] = useState<authStatus>("anonymous");
+  const [statusMessage, setStatusMessage] = useState<string>();
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -26,26 +29,38 @@ export const LoginForm = ({ createAccount }: LoginFromProps) => {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(formData)
+      body: JSON.stringify(formData),
+      credentials: "include",
+
     });
     const data = await res.json();
+
+
+    if (data.username) {
+        setUserStatus('failed')
+      setStatusMessage(data.message)
+    } else {
+      setStatusMessage(data.message)
+      setUserStatus('signed')
+    }
     setIsLoading(false)
-    console.log(data);
   }
 
 
     return (
       <>
-       <div className="py-6 px-6 lg:px-8 text-left">
+          <div className="mx-auto py-6 px-6 lg:px-8 text-left">
 
-          {isloading && <div className="text-center">
+          {isLoading && <div className="text-center">
             <h3 className="mb-4 text-xl font-medium text-gray-900">
               Signing in to VideoDownloader!
             </h3>
             <Loader/>
           </div>}
-
-         {!isloading &&
+         {userStatus === 'signed' &&  <div>
+           <p className="text-green-400 text-center text-xl font-bold">{statusMessage}</p>
+         </div>}
+         {!isLoading && userStatus !== 'signed' &&
         <form
           className="space-y-6"
         onSubmit={handleSubmit}>
@@ -58,6 +73,7 @@ export const LoginForm = ({ createAccount }: LoginFromProps) => {
               type="email"
               name="username"
               id="username"
+              value={formData.username}
               onChange={handleChange}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="JohnyDoe@video.com"
@@ -77,11 +93,11 @@ export const LoginForm = ({ createAccount }: LoginFromProps) => {
               placeholder="••••••••"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               required />
-
             <a href="#" className="text-sm text-blue-700 hover:underline">
               Lost Password?
             </a>
           </div>
+          {userStatus === 'failed' &&  <p className="text-red-400 text-center font-bold">{statusMessage}</p>}
           <button
             type="submit"
             className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,21 +1,25 @@
-export const RegisterForm = () => {
+interface RegisterFormProps {
+  signIn: () => void;
+}
+
+export const RegisterForm = ({signIn}:RegisterFormProps) => {
   return (
     <div className="py-6 px-6 lg:px-8 text-left">
       <h3 className="mb-4 text-xl font-medium text-gray-900">
-        Sign in to VideoDownloader!
+        Create new Account
       </h3>
       <form className="space-y-6">
         <div>
-          <label htmlFor="username"
+          <label htmlFor="email"
                  className="block mb-2 text-sm font-medium text-gray-900">
-            Your username:
+            Your email address:
           </label>
           <input
             type="text"
             name="username"
             id="username"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="JohnyDoe"
+            placeholder="JohnyDoe@video.com"
             required
           />
         </div>
@@ -31,19 +35,18 @@ export const RegisterForm = () => {
             placeholder="••••••••"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             required />
-
-          <a href="#" className="text-sm text-blue-700 hover:underline">
-            Lost Password?
-          </a>
         </div>
         <button
           type="submit"
           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-          Login to your account!
+          Register new account!
         </button>
-        <div className="text-sm font-medium text-gray-500">
-          Not registered?{" "}
-        </div>
+        <a
+          onClick={signIn}
+          className="cursor-pointer block w-full text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+          Return to sign in!
+        </a>
+
       </form>
     </div>
   )
